1bfdd4c39f439d2f741772eebe73ba7f72be62b5
Shotayo Boluwatife <bolutifeakorey@gmail.com>
2022-09-09 16:27:36 -0700

    task10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main- entry point
+ * Description: print possible combos of 2 digits numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int d1 = 0;
+	int d2;
+
+	while (d1 < 10)
+	{
+		d2 = 0;
+		while(d2 < 10)
+		{
+			if (d1 != d2 && d1 < d2)
+			{
+				putchar(d1 + 48);
+				putchar(d2 + 46);
+
+				if (d1 + d2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}	
+			}
+			d2++;	
+		}	
+		d1++;
+	}
+	putchar('\n');
+
+	return (0);
+}
