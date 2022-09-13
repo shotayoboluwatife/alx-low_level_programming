a7d273091317be5bdaaefb3c3b4c9186bc829f31
Shotayo Boluwatife <bolutifeakorey@gmail.com>
2022-09-13 14:32:56 -0700

    tasjek

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _islower - checks for alphabetic character 
+ * _isalpha - checks for alphabetic character 
  * @c: takes in a character
  * Return: 1 if lowercase, letter, uppercase; 0 otherwise
  */
-int _islower(int c)
+int _isalpha(int c)
 {
-        if (c >= 'a' && c <= 'z')
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
         {
                 return (1);
         }
