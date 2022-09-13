56c7da19e01fba80f50224bbd99aef1b431ea125
shotayoboluwatife <111024534+shotayoboluwatife@users.noreply.github.com>
2022-09-13 22:54:22 +0100

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isalpha - checks for alphabetic character 
+ * _isalpha - checks for alphabetic character
  * @c: takes in a character
  * Return: 1 if lowercase, letter, uppercase; 0 otherwise
  */
