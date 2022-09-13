582ff2355a3163a72a3b4b53f24a040815578cf3
Shotayo Boluwatife <bolutifeakorey@gmail.com>
2022-09-13 10:01:23 -0700

    work2

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,17 +7,15 @@
 int main(void)
 {
  /* storing the word to print as an array*/
-        char word_array[] = "_putchar";
-
-        int c;
-
-        /* 8 is used cause of index starts from 0*/
-        /* so each letter would be printed 1by1*/
-        for (c = 0; c < 8; c++)
-        {
-                _putchar(word_array[0]);
-        }
-        _putchar('\n');
-
+	char word_array[] = "_putchar";
+	int c;
+	
+	/* 8 is used cause of index starts from 0*/
+	/* so each letter would be printed 1by1*/
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(word_array[0]);
+	}
+	_putchar('\n');
 	return (0);
 }
