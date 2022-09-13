cb07ebf4d2a690904e702a2bc359566e47916909
Shotayo Boluwatife <bolutifeakorey@gmail.com>
2022-09-13 14:00:12 -0700

    task3

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,6 +3,7 @@
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(int c);
 
 #endif
 /* These are the function declarations for the functions used in all the task */
