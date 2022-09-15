d11e1098c3bfdedeac7ddcb584b16f623afa9b8d
Shotayo Boluwatife <bolutifeakorey@gmail.com>
2022-09-15 10:19:10 -0700

    go fess

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ *
+ * Return: 1 if uppercase and 0 otherwise
+ */
+int _isuppercase(int c)
+{
+	char c;
+
+	if (c >= 'A' && c <= 'Z')
+	{
+
